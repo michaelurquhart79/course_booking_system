6e01f95f4b3c4ab7fe31aa644c7b4441cea4643b
Michael Urquhart <michaelurquhart@hotmail.com>
2019-08-08 15:55:03 +0100

    Custome route added for find customers by course query.

--- src/main/java/com/codeclan/example/CourseBooking/controllers/CourseController.java
+++ src/main/java/com/codeclan/example/CourseBooking/controllers/CourseController.java
@@ -2,7 +2,9 @@ package com.codeclan.example.CourseBooking.controllers;
 
 
 import com.codeclan.example.CourseBooking.models.Course;
+import com.codeclan.example.CourseBooking.models.Customer;
 import com.codeclan.example.CourseBooking.repositories.CourseRepository.CourseRepository;
+import com.codeclan.example.CourseBooking.repositories.CustomerRepository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,10 +20,19 @@ public class CourseController {
     @Autowired
     CourseRepository courseRepository;
 
+    @Autowired
+    CustomerRepository customerRepository;
+
     @GetMapping(value = "/rating/{rating}")
     public List<Course> getCoursesByRating(@PathVariable int rating) {
         return courseRepository.findCourseByRating(rating);
     }
+
+    @GetMapping(value = "/customers/{courseid}")
+    public List<Customer> getCustomersByCourseID(@PathVariable Long courseid) {
+        return customerRepository.findCustomersByCourse(courseid);
+    }
+
 
 
 
